5c3792ad84f2b71a9d9ee66fa5ca214cb194ca9c
Sam <sameisenstat@yahoo.com>
2013-06-20 21:23:09 -0700

    Modify track model to get songs from Apple's RSS.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,16 +1,27 @@
 class Track < ActiveRecord::Base
+  validates_presence_of :id_from_vendor, :name
+
   def self.create_tracks(options={})
-    options.reverse_merge!(:media => :music)
-
-    url = MakeURL.make_url('https://itunes.apple.com/search', options)
-    response = RestClient.get(url)
-    track_info = JSON.parse(response).with_indifferent_access[:results]
-
+    # TODO: Optimize this and user.rb with multi-inserts
+    # TODO: Better error handling and organization
     transaction do
-      # TODO optimize this and user.rb with multi-inserts
-      track_info.map do |params|
-        create_from_vendor_info(params)
-      end
+      feed_url = 'https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf=143441/limit=100/explicit=true/rss.xml'
+      feed = Nokogiri::XML(RestClient.get(feed_url))
+
+      track_ids = feed.xpath('//item//link').map do |collection_url|
+        collection_id = /id(\d*)/.match(collection_url)[1]
+
+        collection_api_url = MakeURL.make_url(
+          'https://itunes.apple.com/lookup',
+          :id => collection_id, :entity => :song)
+        response = RestClient.get(collection_api_url)
+        results = JSON.parse(response).with_indifferent_access[:results]
+        first_track_info = results.select {|info| info[:wrapperType] == 'track' }.first
+
+        if first_track_info
+          create_from_vendor_info(first_track_info)
+        end
+      end.reject(&:nil?)
     end
   end
 
