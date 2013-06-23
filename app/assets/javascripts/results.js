2941ae856b3a82ad6caf7fdd35f36d1dbc58b770
Sam <sameisenstat@yahoo.com>
2013-06-22 23:37:59 -0700

    Let the user play and pause song previews intuitively.
    Also use `on' for event handlers, for when we add infinite scrolling.

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -1,35 +1,60 @@
 $(function(){
 	$('.twitter-popup').click(function(event) {
-    var width  = 550,
-        height = 400,
-        left   = ($(window).width()  - width)  / 2,
-        top    = ($(window).height() - height) / 2,
-        url    = this.href,
-        opts   = 'status=1' +
-                 ',width='  + width  +
-                 ',height=' + height +
-                 ',top='    + top    +
-                 ',left='   + left;
+		var width  = 550,
+			height = 400,
+			left   = ($(window).width()  - width)  / 2,
+			top    = ($(window).height() - height) / 2,
+			url    = this.href,
+			opts   = 'status=1' +
+			         ',width='  + width  +
+			         ',height=' + height +
+			         ',top='    + top    +
+			         ',left='   + left;
 
-    window.open(url, 'twitter', opts);
+		window.open(url, 'twitter', opts);
 
-    return false;
-  });
+		return false;
+	});
 
-  $('.facebook-popup').click(function(){
-  	window.open(
-      'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),
-      'fb-share-dialog',
-      'width=626,height=436');
+	$('.facebook-popup').click(function(){
+		window.open(
+			'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),
+			'fb-share-dialog',
+			'width=626,height=436');
 
-    return false;
-  });
+		return false;
+	});
 
-  $('.js-play-preview').click(function(e){
-    e.preventDefault();
-    $(this).find('.icon-play-circled').toggleClass('icon-play-circled icon-pause-circled');
-    var audio = $(this).find('audio');
-    audio.get(0).play();
-  });
+	var previewPlaying = null;
+	var lastPreviewPlayed = null;
 
+	$('.track-list').on('click', '.js-play-preview', function(e){
+		e.preventDefault();
+
+		if(!$(this).is(previewPlaying)) {
+			if(lastPreviewPlayed != null && !$(this).is(lastPreviewPlayed)) {
+				stopPreview(lastPreviewPlayed);
+				lastPreviewPlayed.find('audio')[0].currentTime = 0;
+			}
+			playPreview($(this));
+			previewPlaying = lastPreviewPlayed = $(this);
+		} else {
+			stopPreview($(this));
+			previewPlaying = null;
+		}
+	});
+
+	$('.track-list').on('ended', '.js-play-preview', function() {
+		stopPreview($(this));
+	});
+
+	var playPreview = function(tag) {
+		tag.find('.icon-play-circled').toggleClass('icon-play-circled icon-pause-circled');
+		tag.find('audio')[0].play();
+	};
+
+	var stopPreview = function(tag) {
+		tag.find('.icon-pause-circled').toggleClass('icon-play-circled icon-pause-circled');
+		tag.find('audio')[0].pause();
+	};
 });
