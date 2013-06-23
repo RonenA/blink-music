942c93b9c4a687a0d2a1ceddfe4c78bf182c4c91
Ronen Ackerman <ronen.ackerman@gmail.com>
2013-06-23 04:34:09 -0700

    Skip button to blast (currently always hidden) and google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ BlinkMusic::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #Google Analytics
+  GA.tracker = "UA-41950164-1"
 end
